5833f6eb0a2c7e560a80bfddd0db1be474f8596c
taehyun0411 <kimth0411a@gmail.com>
2024-05-15 16:29:36 +0900

    Changes

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,19 +1,5 @@
-import { sql } from "@vercel/postgres";
-
-export default async function Cart({
-  params
-} : {
-  params: { user: string }
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
-
+export default function Home() {
   return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
+      <p>Hello</p>
   );
 }
